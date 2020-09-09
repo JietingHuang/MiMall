0c627ba1055ee5f7c4f52cd9edcac256f95c9c4d
JietingHuang <541975984@qq.com>
2020-09-10 02:58:01 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <router-view></router-view>
     <router-view/>
   </div>
 </template>
@@ -32,25 +29,19 @@
 </style>
 
 <script>
-// import axios from 'axios'
 import jsonp from 'jsonp'
 export default {
+  name: 'app',
+  components: {
+
+  },
   data(){
     return{
-      age: 24,
-      data: ''
+      
     }
   },
   mounted(){
-    let url = "/api/kg/getgraph";
-    // axios.get(url).then(() => {
-
-    // })
-
-    jsonp(url, (err, res)=>{
-      let result = res;
-      this.data = result;
-    })
+    
   }
 }
 </script>
