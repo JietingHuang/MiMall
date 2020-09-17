acbb2a94b1a668513a613b2243bc9512fd6483d9
JietingHuang <541975984@qq.com>
2020-09-17 14:18:12 +0800

    接口环境设置

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 // webpack配置表
 
+// 接口代理方式
 module.exports = {
     devServer: {
         host: 'localhost',
-        port: 8081,
+        port: 8080,
         proxy: {
             '/api': {
-                target: 'http://www.miaoleyan.com',
+                target: 'http://www.imooc.com',
                 changeOrigin: true,
                 pathRewrite: {
                     '/api': ''
