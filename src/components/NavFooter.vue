0cc54a08758fa95b0b8bbd7531ad77e363b008c9
JietingHuang <541975984@qq.com>
2020-09-22 18:33:11 +0800

    5-6 Nav-Footer组件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="">河畔一角主页</a><span>|</span>
+            <a href="">Vue全栈课程</a><span>|</span>
+            <a href="">React全家桶课程</a><span>|</span>
+            <a href="">微信支付专项课程</a><span>|</span>
+            <a href="">配套课程：Java通用型支付+电商平台双系统实战</a>
+        </div>
+        <div class="copyright">Copyright ©2020 <span class="domain">mi.futurefe.com</span> All Rights Reserved.</div>
     </div>
 </template>
 
@@ -9,4 +20,39 @@ export default {
     name: 'nav-footer',
 }
 </script>
+
+<style lang="scss">
+    .footer{
+        height: 234px;
+        border-top: 1px solid #FF6600;
+        background-color: #333;
+        color: #999;
+        font-size: 16px;
+        text-align: center;
+        .footer-logo{
+            margin-top: 46px;
+            margin-bottom: 31px;
+            img{
+                width: 53px;
+                height: 36px;
+                margin-bottom: 13px;
+            }
+        }
+        .footer-link{
+            a{
+                color: #999;
+                display: inline-block;
+            }
+            span{
+                margin: 0 10px;
+            }
+            margin-bottom: 13px;
+        }
+        .copyright{
+            .domain{
+                color: #FF6600;
+            }
+        }
+    }
+</style>
 
