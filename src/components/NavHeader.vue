1dc3c10faadf6861bd4631c01150aa57c708e455
JietingHuang <541975984@qq.com>
2020-09-26 23:53:02 +0800

    修改一下层级 z-index

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -246,6 +246,7 @@ export default {
                             overflow: hidden;
                             border-top: 1px solid #E5E5E5;
                             box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
+                            z-index: 10;
                             transition: all .5s;
                             background-color: #fff;
                             .product{
