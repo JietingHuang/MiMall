a58f4bd7558bcd873bcf1703377a91ec9bf9b434
JietingHuang <541975984@qq.com>
2020-11-05 22:51:15 +0800

    首页广告位

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -59,8 +59,16 @@
                     <div class="swiper-button-next" slot="button-next"></div>
                 </swiper>
             </div>
-            <div class="ads-box"></div>
-            <div class="banner"></div>
+            <div class="ads-box">
+                <a href="'/#/product/'+item.id" v-for="(item,index) in adsList" v-bind:key="index">
+                    <img v-bind:src="item.img" alt="">
+                </a>
+            </div>
+            <div class="banner">
+                <a href="/#/product/30">
+                    <img src="/imgs/banner-1.png" alt="">
+                </a>
+            </div>
             <div class="product-box"></div>
         </div>
         <service-bar></service-bar>
@@ -147,6 +155,21 @@ export default {
                 ], [
                     0,0,0,0
                 ]
+            ],
+            adsList:[
+                {
+                    id:33,
+                    img:'/imgs/ads/ads-1.png'
+                }, {
+                    id:48,
+                    img:'/imgs/ads/ads-2.jpg'
+                }, {
+                    id:45,
+                    img:'/imgs/ads/ads-3.png'
+                }, {
+                    id:47,
+                    img:'/imgs/ads/ads-4.jpg'
+                },
             ]
         }
     }
@@ -234,6 +257,20 @@ export default {
                     height: 100%;
                 }
             }
+        }
+        .ads-box{
+            // display: flex;
+            // justify-content: space-between;
+            @include flex();
+            margin-top: 14px;
+            margin-bottom:31px;
+            a{
+                width: 296px;
+                height: 167px;
+            }
+        }
+        .banner{
+            margin-bottom: 50px;
         }
     }
 </style>
